29599c5d7cc5625e8568d1a7a69362315e28e824
KelinDrawn <toni.schlaeppi@icloud.com>
2024-10-22 16:04:07 +0200

    fixes55

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kelindrawn.github.io",
-  base: "Toni_Portfolio",
-
-  vite: {
-    plugins: [
-      {
-        name: "svg-loader",
-        enforce: "pre",
-        load(id) {
-          if (id.endsWith(".svg")) {
-            return `export default ${JSON.stringify(id)}`;
-          }
-        },
-      },
-    ],
-  },
+  site: "https://toni-ux.dev",
 });
